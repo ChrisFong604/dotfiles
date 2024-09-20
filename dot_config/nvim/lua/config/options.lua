a608ec63127bf222d5c4da8516c3a64b42bc173e
ChrisFong604 <ccffoonngg@gmail.com>
2024-09-19 22:32:02 -0700

    changed to default lazy.nvim template with slight modifications

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local o = vim.o
+o.winbar = "%=%m %f"
+o.textwidth = 80
+o.wrap = true
+o.linebreak = true
--- dot_config/nvim/lua/options.lua
+++
@@ -1,9 +0,0 @@
-require "nvchad.options"
-
--- add yours here!
-
-local o = vim.o
-o.textwidth = 80
-o.wrap = true 
-o.linebreak = true
--- o.cursorlineopt ='both' -- to enable cursorline!
